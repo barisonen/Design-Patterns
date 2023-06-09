baf5d4c4e55902902d07645fd1df8d60a0984a1f
barisonen <barisonen.1996@gmail.com>
2023-06-09 22:41:41 +0300

    Update VehicleFactory.java

--- Factory/VehicleFactory.java
+++ Factory/VehicleFactory.java
@@ -1,17 +1,11 @@
 public class VehicleFactory {
 
     public Vehicle getVehicle(VehicleType vehicleType) {
-        if (vehicleType == null) {
-            return null;
-        }
-        else if (vehicleType.equals(VehicleType.CAR)) {
+        if (VehicleType.CAR.equals(vehicleType)) {
             return new Car();
         }
-        else if (vehicleType.equals(VehicleType.PLANE)) {
-            return new Plane();
-        }
         else {
-            return null;
+            return new Plane();
         }
     }
 }
